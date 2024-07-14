667653ac829fb5a3de9508ee5c9c2b8ff1fd206e
Kristian Quirapas <kmquirapas@gmail.com>
2024-07-14 19:24:07 +0800

    removed wasm

--- programs/token_sale/src/lib.rs
+++ programs/token_sale/src/lib.rs
@@ -5,6 +5,5 @@ pub mod macros;
 pub mod merkle;
 pub mod processor;
 pub mod state;
-pub mod wasm;
 
 solana_program::declare_id!("Aq2EAZ8i8UgKGaGzpSPhfvGxf4hkziymA4WqXrJ4NYu4");
--- programs/token_sale/src/wasm.rs
+++
@@ -1,9 +0,0 @@
-// /// WASM interop for consistent proof generation algorithm
-// /// Built via `wasm-pack`
-// use merkletreers::{node::Node, Leaf};
-// use wasm_bindgen::prelude::*;
-//
-// #[wasm_bindgen]
-// pub fn get_member_proof(leaves: Vec<Leaf>) -> Vec<Node> {
-//     vec![]
-// }
